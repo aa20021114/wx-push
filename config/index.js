fd45ac53b843c4b7ff511dc1d1bf5fb66f8bac13
aa20021114 <142730447+aa20021114@users.noreply.github.com>
2023-08-21 22:53:26 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wx6f137bf4de067f10",
+  appsecret: "3a4c87a13a139bff5f6139d5f1a3a607",
+  template_id: "DQ6wHZCymRLoyAr65j5Dlkcrl_Qx_qZazpM5K6WDU5M",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "保定",
+  first_date: "2023-07-26",
 };
